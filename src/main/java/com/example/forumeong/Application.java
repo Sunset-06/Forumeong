90359fa2416b3bdbb393743a066c2b28f8e85dff
Sunset-06 <nilay.sabnis05@gmail.com>
2025-01-29 07:03:41 +0530

    synced with client code

--- src/main/java/com/example/forumeong/Application.java
+++ src/main/java/com/example/forumeong/Application.java
@@ -1,10 +1,8 @@
 package com.example.forumeong;
 
 import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 @SpringBootApplication
 public class Application {
